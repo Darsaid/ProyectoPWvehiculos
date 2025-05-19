f988845816f457710c80a0ecb09dc3294656de19
DANIEL RESTREPO VILLA <daniel.restrepo.42@gmail.com>
2025-05-18 23:06:50 -0500

    Creacion Modelo, Vista de contacto

---
+++ ProyectoPWvehiculos/Models/ContactoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoPWvehiculos.Models
+{
+    public class ContactoViewModel
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Ingrese un correo válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El mensaje no puede estar vacío.")]
+        [StringLength(500, ErrorMessage = "El mensaje no puede superar los 500 caracteres.")]
+        public string Mensaje { get; set; }
+    }
+}
